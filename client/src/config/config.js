9fbfd63100d7ae84de7cf14ac6e8235d7f2d04b4
saikat22071995 <sunny22071995@gmail.com>
2019-12-24 21:26:52 +0530

    url fixed

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
 const axios=Axios.create({
-    baseURL:'https://ticket-master-rakshith.herokuapp.com/api'
+    baseURL:'https://ticket-master-saikat.herokuapp.com/api'
 })
 export default axios
